3412c1d6459836ca5dc6b68c353639a46b85f1ad
Alexandr Tolstukhin <alex.tolstuchin@gmail.com>
2022-04-05 18:57:28 +0500

    add YARP

--- src/Gateway/MicroservicesFeed.Gateway/Program.cs
+++ src/Gateway/MicroservicesFeed.Gateway/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("YARP"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Microservices Feed Gateway");
+app.MapReverseProxy();
 
 app.Run();
